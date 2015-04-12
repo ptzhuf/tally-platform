95b3198ca7eccf052c225b0e550032dd00f5b901
ptzhuf <251794519@qq.com>
2015-04-13 00:02:25 +0800

    错误处理调试

--- tally-web/src/main/java/org/ringr/tally/controller/ExcecptionHandlingController.java
+++ tally-web/src/main/java/org/ringr/tally/controller/ExcecptionHandlingController.java
@@ -8,9 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 import org.ringr.tally.exception.CommonException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 全局异常处理.
@@ -28,12 +30,18 @@ public class ExcecptionHandlingController {
 			.getLogger(ExcecptionHandlingController.class);
 
 	@ExceptionHandler(CommonException.class)
-	public String handlingNotFound(HttpServletRequest req, Exception exception,
-			Model model) {
+	public ModelAndView handlingNotFound(HttpServletRequest req,
+			Exception exception) {
 		LOG.error("Request: " + req.getRequestURL() + " raised " + exception);
-
+		ModelAndView view = new ModelAndView("error/404");
+		ModelMap model = view.getModelMap();
 		model.addAttribute("exception", exception);
 		model.addAttribute("url", req.getRequestURL());
+		return view;
+	}
+
+	@RequestMapping("/error")
+	public String error() {
 		return "error/404";
 	}
 }
