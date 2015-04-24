f2d375d79c0e4877526fd08c5abdcb722d7efaa9
ptzhuf <494237784@qq.com>
2015-04-24 18:53:55 +0800

    修改~

---
+++ tally-web/src/main/java/org/ringr/tally/controller/admin/UserAdminController.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.ringr.tally.controller.admin;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author ptzhuf
+ *
+ */
+@Controller
+public class UserAdminController {
+
+}
